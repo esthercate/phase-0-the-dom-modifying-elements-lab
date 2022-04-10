643a6708707f1bd6151ece13f9818df821fb1968
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-04-10 14:59:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const removeMain = document.querySelector('main');
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Catheine is the champion";
+
